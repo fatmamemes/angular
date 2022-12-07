7313de43649ffa1d99ac80e28395c5b69199a817
najjj <najjjjar83@gmail.com>
2022-12-07 12:59:09 +0300

    http and service -abdurrahman

--- src/app/fatma/basics/basics.module.ts
+++ src/app/fatma/basics/basics.module.ts
@@ -4,20 +4,27 @@ import {RouterModule, Routes} from "@angular/router";
 import {SharedModule} from "../../shared/shared.module";
 import { ListProductComponent } from './list-product/list-product.component';
 import { ProductComponent } from './product/product.component';
+import { HttptrainningComponent } from './httptrainning/httptrainning.component';
+import {BasicService} from "./basic.service";
+import {HttpClientModule} from "@angular/common/http";
 
 export const routes: Routes = [
-  { path: 'list-product', component: ListProductComponent, data: { breadcrumb: 'list-product' } },
+  // { path: 'list-product', component: ListProductComponent, data: { breadcrumb: 'list-product' } },
+  {path: '', component: HttptrainningComponent, data: { breadcrumb: 'Http-training' } },
 ];
 
 @NgModule({
   declarations: [
     ListProductComponent,
-       ProductComponent
+       ProductComponent,
+       HttptrainningComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    SharedModule
-  ]
+    SharedModule,
+    HttpClientModule
+  ],
+  providers: [BasicService]
 })
 export class BasicsModule { }
